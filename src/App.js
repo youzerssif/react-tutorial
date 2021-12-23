6ea07c2bc3bcbcf81f2884d38852795bb7fdd570
SAMAKE MOHAMED <39623776+youzerssif@users.noreply.github.com>
2021-12-23 13:08:05 +0100

    get

--- src/App.js
+++ src/App.js
@@ -1,69 +1,69 @@
 import React, {Component} from 'react'
-// import Table from './Table'
-// import Form from './Form'
+import Table from './Table'
+import Form from './Form'
 // import App from './Api';
 
 
-class App extends Component {
-  state = {
-    data: [],
-  }
+// class App extends Component {
+//   state = {
+//     data: [],
+//   }
 
-  // Code is invoked after the component is mounted/inserted into the DOM tree.
-  componentDidMount() {
-    const url =
-      'https://en.wikipedia.org/w/api.php?action=opensearch&search=Seona+Dancing&format=json&origin=*'
+//   // Code is invoked after the component is mounted/inserted into the DOM tree.
+//   componentDidMount() {
+//     const url =
+//       'https://en.wikipedia.org/w/api.php?action=opensearch&search=Seona+Dancing&format=json&origin=*'
 
-    fetch(url)
-      .then((result) => result.json())
-      .then((result) => {
-        this.setState({
-          data: result,
-        })
-      })
-  }
+//     fetch(url)
+//       .then((result) => result.json())
+//       .then((result) => {
+//         this.setState({
+//           data: result,
+//         })
+//       })
+//   }
 
-  render() {
-    const {data} = this.state
+//   render() {
+//     const {data} = this.state
 
-    const result = data.map((entry, index) => {
-      return <li key={index}>{entry}</li>
-    })
-    return <ul>{result}</ul>
-  }
-}
-
-export default App
-// class App extends Component{
-    
-//     state = {
-//         characters: [
-            
-//         ],
-//       }
-//     removeCharacter = (index) => {
-//     const {characters} = this.state
-    
-//     this.setState({
-//         characters: characters.filter((character, i) => {
-//         return i !== index
-//         }),
+//     const result = data.map((entry, index) => {
+//       return <li key={index}>{entry}</li>
 //     })
-//     }
-
-//     handleSubmit = (character) => {
-//         this.setState({characters: [...this.state.characters, character]})
-//     }
-
-//     render(){
-//         const { characters } = this.state
-//         return(
-//             <div className="App">
-//                 <Table characterData={characters} removeCharacter = {this.removeCharacter}/>
-//                 <Form handleSubmit={this.handleSubmit} />
-//             </div>
-//         )
-//     }
+//     return <ul>{result}</ul>
+//   }
 // }
 
 // export default App
+class App extends Component{
+    
+    state = {
+        characters: [
+            
+        ],
+      }
+    removeCharacter = (index) => {
+    const {characters} = this.state
+    
+    this.setState({
+        characters: characters.filter((character, i) => {
+        return i !== index
+        }),
+    })
+    }
+
+    handleSubmit = (character) => {
+        this.setState({characters: [...this.state.characters, character]})
+    }
+
+    render(){
+        const { characters } = this.state
+        return(
+            <div className="App">
+                <Table characterData={characters} removeCharacter = {this.removeCharacter}/>
+                <Form handleSubmit={this.handleSubmit} />
+            </div>
+        )
+    }
+}
+
+export default App
